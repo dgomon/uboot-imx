afe4b10d3d59b0e41373d464bfd84371d5a2b847
Kirill Kapranov <kirill.kapranov@compulab.co.il>
2020-05-28 23:13:18 +0300

    Migration to rel_imx_5.4.3_2.0.0
    Signed-off-by: Kirill Kapranov <kirill.kapranov@compulab.co.il>

--- board/compulab/ucm-imx8m-mini/spl.c
+++ board/compulab/ucm-imx8m-mini/spl.c
@@ -19,7 +19,7 @@
 #include <asm/mach-imx/mxc_i2c.h>
 #include <fsl_esdhc.h>
 #include <mmc.h>
-#include <asm/arch/imx8m_ddr.h>
+#include <asm/arch/ddr.h>
 #include "ddr/ddr.h"
 #include "ucm-imx8m-mini.h"
 
@@ -99,6 +99,7 @@ int board_mmc_init(bd_t *bis)
 	for (i = 0; i < CONFIG_SYS_FSL_USDHC_NUM; i++) {
 		switch (i) {
 		case 0:
+			init_clk_usdhc(1);
 			usdhc_cfg[0].sdhc_clk = mxc_get_clock(MXC_ESDHC2_CLK);
 			imx_iomux_v3_setup_multiple_pads(
 				usdhc2_pads, ARRAY_SIZE(usdhc2_pads));
@@ -108,6 +109,7 @@ int board_mmc_init(bd_t *bis)
 			gpio_direction_output(USDHC2_PWR_GPIO, 1);
 			break;
 		case 1:
+			init_clk_usdhc(2);
 			usdhc_cfg[1].sdhc_clk = mxc_get_clock(MXC_ESDHC3_CLK);
 			imx_iomux_v3_setup_multiple_pads(
 				usdhc3_pads, ARRAY_SIZE(usdhc3_pads));
@@ -217,10 +219,12 @@ int board_fit_config_name_match(const char *name)
 void board_init_f(ulong dummy)
 {
 	int ret;
+	/* Clear the BSS. */
+	memset(__bss_start, 0, __bss_end - __bss_start);
 
-	/* Clear global data */
+	/* Clear global data
 	memset((void *)gd, 0, sizeof(gd_t));
-
+	*/
 	arch_cpu_init();
 
 	board_early_init_f();
@@ -228,9 +232,6 @@ void board_init_f(ulong dummy)
 	timer_init();
 
 	preloader_console_init();
-
-	/* Clear the BSS. */
-	memset(__bss_start, 0, __bss_end - __bss_start);
 
 	ret = spl_init();
 	if (ret) {
